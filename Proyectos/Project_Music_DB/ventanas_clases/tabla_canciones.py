4f661017d0b124277f698ffe15cc841d8e749c78
Silvia Sanjosé <sanjose.silvia@gmail.com>
2020-04-07 17:38:03 +0200

    Add files via upload

--- Proyectos/Project_Music_DB/ventanas_clases/tabla_canciones.py
+++ Proyectos/Project_Music_DB/ventanas_clases/tabla_canciones.py
@@ -1,12 +1,11 @@
 from ventanas.ventana_tabla_listado import Ui_tabla_canciones
 from modelo.operacionesDB import listar_canciones_lista
 from PyQt5.Qt import QTableWidgetItem
+from functools import partial
 
 class Tabla_Canciones(Ui_tabla_canciones):
     def setupUi(self, my_win):
         super().setupUi(my_win)
-        
-        
         
         canciones = listar_canciones_lista()
         fila = 0
@@ -19,6 +18,12 @@ class Tabla_Canciones(Ui_tabla_canciones):
                 columna +=1
             fila += 1
             
-            
+        self.btn_editar.clicked.connect(partial(self.editar_cancion, c[0])  )
+        self.btn_eliminar.clicked.connect(self.eliminar_cancion)
         
         
+    def editar_cancion(self, ed):
+        print("HOla caracola", ed)
+        
+    def eliminar_cancion(self):
+        print("HOla caracola")
