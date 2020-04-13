cf9b6e53926657a3514811326c53b5f27ff7b1d4
Silvia Sanjosé <sanjose.silvia@gmail.com>
2020-04-13 18:01:37 +0200

    actualizo con fecha y hora

--- Proyectos/Project_Music_DB/ventanas_clases/bienvenidos.py
+++ Proyectos/Project_Music_DB/ventanas_clases/bienvenidos.py
@@ -1,5 +1,5 @@
 from ventanas.ventana_bienvenida import Ui_music_window
-
+from PyQt5.Qt import QTime, QDate
 
 
 class VentanaMusical(Ui_music_window):
@@ -14,16 +14,28 @@ class VentanaMusical(Ui_music_window):
         self.sub_menu_registro.triggered.connect(self.mostrar_registrar_cancion)
         self.sub_meni_listado_canciones.triggered.connect(self.mostrar_listado_canciones)
         self.sub_menu_tabla_Canciones.triggered.connect(self.mostrar_tabla_canciones)
+        self.sub_menu_inicio.aboutToShow.connect(self.volver_incio)
+        
+        date = QDate.currentDate()
+        date = str(date.toPyDate())
+        self.txt_fecha.setText(date)
+          
+        hora = QTime.currentTime()
+        hora_txt = hora.toString("hh:mm")
+        self.lcdNumber.display(hora_txt)   
         
     def mostrar_registrar_cancion(self):
         self.registro.setupUi(self.my_win)
         
     def mostrar_listado_canciones(self):
         self.listado.setupUi(self.my_win)
-        
+          
     def mostrar_tabla_canciones(self):
         self.tabla.setupUi(self.my_win)
     
+    def volver_incio(self):
+        self.setupUi(self.my_win)
+        
    
         
     
--- Proyectos/Project_Music_DB/ventanas_clases/tabla_canciones.py
+++ Proyectos/Project_Music_DB/ventanas_clases/tabla_canciones.py
@@ -10,11 +10,11 @@ class Tabla_Canciones(Ui_tabla_canciones):
     def __init__(self, my_win, editar):
         self.editar = editar
         self.my_win = my_win
-        
+
     def setupUi(self, my_win):
         super().setupUi(my_win)
-        #self.my_win = my_win
-        
+        self.my_win = my_win
+
         #creación la tabla
         canciones = listar_canciones_lista()
         fila = 0
@@ -26,10 +26,9 @@ class Tabla_Canciones(Ui_tabla_canciones):
                 self.tableWidget.setItem(fila, columna, celda)
                 columna +=1
             fila += 1
-            
+
         self.btn_editar.clicked.connect(partial(self.editar_eliminar_cancion, "editar"))
         self.btn_eliminar.clicked.connect(partial(self.editar_eliminar_cancion, "eliminar"))
-        
 
     def editar_eliminar_cancion(self, accion):
         val_id = self.spx_numero_id.value()
@@ -39,13 +38,13 @@ class Tabla_Canciones(Ui_tabla_canciones):
             if accion == "eliminar":
                 eliminar = QMessageBox.question(self.my_win, "Alerta", "¿Quieres eliminar esa canción?")
                 if eliminar == QMessageBox.Yes:
-                    borrar_cancion(val_id)       
+                    borrar_cancion(val_id)
                     self.setupUi(self.my_win)
             elif accion == "editar":
                 editar = QMessageBox.question(self.my_win, "Alerta", "¿Quieres modificar esa canción?")
                 if editar == QMessageBox.Yes:
                     self.editar_cancion(val_id)
- 
+
     def editar_cancion(self,id_editar):
         self.editar.setupUi(self.my_win)
         cancion_a_editar = obtener_cancion_por_id(id_editar)
@@ -55,7 +54,7 @@ class Tabla_Canciones(Ui_tabla_canciones):
         self.editar.txt_anio.setText(str(cancion_a_editar.anio))
         self.editar.txt_estilo.setText(cancion_a_editar.estilo)
         self.editar.btn_cambiar.clicked.connect(partial(self.guardar_cambios_cancion,cancion_a_editar.id))
-        
+
     def guardar_cambios_cancion(self, id_guardar):
         cancion_editada = Cancion()
         cancion_editada.id = id_guardar
@@ -64,14 +63,18 @@ class Tabla_Canciones(Ui_tabla_canciones):
         cancion_editada.album = self.editar.txt_album.text()
         cancion_editada.anio = self.editar.txt_anio.text()
         cancion_editada.estilo = self.editar.txt_estilo.text()
+
         guardar_cambios_cancion_db(cancion_editada)
+
         QMessageBox.about(self.my_win, "Info", "Cambios Guardados")
-        self.setupUi(self.my_win)
        
-        
-        
-           
-        
-        
-        
-        
+        self.setupUi(self.my_win)
+
+
+
+
+
+
+
+
+
