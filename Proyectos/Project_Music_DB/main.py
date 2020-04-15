32ce6206dfbf6cc36b7cab8bfb89660b46735dbe
Silvia Sanjosé <sanjose.silvia@gmail.com>
2020-04-15 18:27:55 +0200

    actualizo con nueva bbdd y opciones

--- Proyectos/Project_Music_DB/main.py
+++ Proyectos/Project_Music_DB/main.py
@@ -5,6 +5,7 @@ from ventanas_clases.listado_canciones import Listado_canciones
 from ventanas_clases.tabla_canciones import Tabla_Canciones
 from ventanas_clases.bienvenidos import VentanaMusical
 from ventanas.ventana_cambio import Ui_editor_window
+from ventanas_clases.registro_usuario import Registro_usuario
 
 
 
@@ -20,9 +21,10 @@ win_registro_cancion = Registrar_cancion()
 win_listado_cancion = Listado_canciones()
 win_editar_cancion = Ui_editor_window()
 win_tabla_canciones = Tabla_Canciones(my_win, win_editar_cancion)
+win_registro_usuarios = Registro_usuario()
 
 
-win_bienvenidos = VentanaMusical(my_win, win_registro_cancion, win_listado_cancion, win_tabla_canciones)
+win_bienvenidos = VentanaMusical(my_win, win_registro_cancion, win_listado_cancion, win_tabla_canciones, win_registro_usuarios)
 win_bienvenidos.setupUi(my_win)
 
 my_win.show()
